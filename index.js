0fc72fa759d6f474f89ec3809c44fc2ac56b81fb
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-08-03 22:12:37 +0700

    recommit

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const path = require("path");
-const favicon = require("express-favicon");
-const cors = require("cors");
 const bodyParser = require("body-parser");
+const favicon = require("express-favicon");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
 
-if (process.env.NODE_ENV === "test") {
-  app.use(morgan("dev"));
-}
+app.use(morgan("dev"));
 app.use(cors());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 app.use(bodyParser.json());
-app.use(favicon(path.join(__dirname, "client-ui", "build", "favicon.ico")));
+app.use(
+  favicon(path.join(__dirname, "..", "client-ui", "build", "favicon.ico"))
+);
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "client-ui", "build")));
-if (!process.env.NODE_ENV === "test") {
+app.use(express.static(path.join(__dirname, "..", "client-ui", "build")));
+if (process.env.NODE_ENV !== "test") {
   app.get("*", function (req, res) {
-    const index = path.join(__dirname, "client-ui", "build", "index.html");
+    const index = path.join(
+      __dirname,
+      "..",
+      "client-ui",
+      "build",
+      "index.html"
+    );
     res.sendFile(index);
   });
 }
